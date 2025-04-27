9e0f9502ddfb7562ca40fb6749bcbacfa8f58389
ls-c0de <137232009+ls-c0de@users.noreply.github.com>
2025-04-27 20:59:06 +0200

    Counting Text works now

--- src/entropie/Counter.java
+++ src/entropie/Counter.java
@@ -4,20 +4,43 @@ public class Counter {
 	private final int MAXPAIRS = 100;
 	private SymbolPair[] arr = new SymbolPair[MAXPAIRS];
 	char[] dough;
+	char[] bread;
 	
 	public void countText(String text) {
-		dough = text.toCharArray();
-
-		for (int i = 0; i < dough.length; i++) {
-			einordnen(dough[i]);
+		String bereinigterText = text.replaceAll("[\\n\\r\\t ]", "");
+		
+		bread = bereinigterText.toCharArray();
+		
+		for (int i = 0; i < bread.length; i++) {
+			einordnen(bread[i]);
 		}
 	}
 
 	public void output() {
-		for (SymbolPair s : arr) {
-			System.out.println(s.toString());
+//		for (SymbolPair s : arr) {
+//			System.out.println(s.toString());
+//		}
+		for (int i = 0;i < arr.length;i++) {
+			System.out.println(arr[i].toString());
+			if (arr[i+1] == null) {
+				break;
+			}
 		}
+		System.out.println("\n\n Zeichen gesamt: " + bread.length);
 	}
+
+//	private void einordnenOLD(char symbol) {
+//		for (int i = 0; i < arr.length; i++) {
+//			if (arr[i].compare(symbol)) {
+//				arr[i].addOne();
+//			}
+//			if (arr[i] == null) {
+//				arr[i].addOne();
+//				break;
+//			} else
+//				arr[i] = new SymbolPair(symbol);
+//		}
+//	}
 	
 	private void einordnen(char symbol) {
 		for (int i = 0; i < arr.length; i++) {
@@ -27,7 +50,9 @@ public class Counter {
 			}
 			if (arr[i].compare(symbol)) {
 				arr[i].addOne();
+				break;
 			}
 		}
 	}
+
 }
